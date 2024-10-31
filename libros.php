6a0d495e2700f813e68da450888dd133940dc8bd
Neri <bosconerina06@gmail.com>
2024-10-31 11:07:17 -0300

    Acomodar para la marti

--- libros.php
+++ libros.php
@@ -53,20 +53,49 @@
         <div class="libro historia">Historia</div>
     </div>
 
-    <div class="comedia">
-        <div class="card mb-3" style="max-width: 540px;">
+    <div class="terrorlibro">
+        <h2>Terror</h2>
+    <div class="card" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-            <img src="./img/Dracula.webpg" class="img-fluid rounded-start">
+            <img src="./img/Dracula.webp" class="portadas">
             </div>
             <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                <h3 class="card-title">Drácula</h3>
+                <p class="card-text">Novela gótica de terror que narra la historia del conde Drácula, un ser solitario y terrorífico que se traslada de Transilvania a Londres para conseguir sus fines.</p>
             </div>
             </div>
         </div>
+        <div class="card" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+            <img src="./img/dentrodemi.webp" class="portadas">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h3 class="card-title">Dentro de mí</h3>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
 
     <script src="./script.js"></script>
