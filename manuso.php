4f64986e943a4c53927704f8663f3a719bbead35
martimil <barnardmartinao@gmail.com>
2024-11-01 09:44:17 -0300

    jajaj neri

--- manuso.php
+++ manuso.php
@@ -18,29 +18,43 @@
             <a href="#contacto">Contacto</a>                    
         </nav>
     </header>
+   
 
-    <h1 class="titulo">Manual de uso</h1>
+    
 
+    <div class="container">
+        <h1 class="titulo">Manual de uso</h1>
     <div class="Preguntas">
- 
-     <h2>¿Qué debo hacer para retirar un libro?</h2>
-      <p>Primero debes registrarte, sino posee cuenta</p>
-
-      <h2>¿Cómo hago para encargar un libro?</h2>
-      <p>Precionas la tecla reservar y eso seria todo ya que se guarda con tu nombre.</p>
-
-      <h2>¿Qué generos estran dispuestos en la biblioteca?</h2>
-      <p>Los libros que esan disponibless son: matemáticas, historia, cinecia ficción, terror, lengua,etc. </p>
-
-      <h2>¿Cómo puedo solicitar un libro que no está disponible?</h2>
-      <p>Lo podes solitar reservando el libro para cuando se desocupe lo puedas sacar.</p>
-
-      <h2>¿Cuáles son las políticas de prestamo y devolución?</h2>
-      En caso de que el libro se devuelva en mal estado por ej: roto, rayado, entre otros. Se le aplicara un actá del colegio y además debera pagar los daños.
-
+     
+      <div class="dividido1">
+        <div class="col-md3">
+         <h2 class="card-title" >¿Qué debo hacer para retirar un libro?</h2>
+        <h5 class="letra">Primero debes registrarte, sino posee cuenta</h5>
+        </div>
+      </div>
+       <div class="col-md3">
+        <h2 class="card-title" >¿Cómo hago para encargar un libro?</h2>
+        <h5 class="letra">Precionas la tecla reservar y eso seria todo ya que se guarda con tu nombre.</h5>
+       </div>
+      <div class="col-md3">
+        <h2 class="card-title">¿Qué generos estran dispuestos en la biblioteca?</h2>
+       <h5 class="letra">Los libros que esan disponibless son: matemáticas, historia, cinecia ficción, terror, lengua,etc. </h5>
     </div>
-
-
+     </div>
+      <div class="dividido2">
+        <div class="col-md3">
+       <h2 class="card-title">¿Cómo puedo solicitar un libro que no está disponible?</h2>
+        <h5 class="letra">Lo podes solitar reservando el libro para cuando se desocupe lo puedas sacar.</h5>
+    </div>
+       <div class="col-md3">
+        <h2 class="card-title">¿Cuáles son las políticas de prestamo y devolución?</h2>
+         <h5 class="letra">En caso de que el libro se devuelva en mal estado por ej: roto, rayado, entre otros.<br>
+         Se le aplicara un actá del colegio y además debera pagar los daños.</h5>
+      </div>
+    </div>
+  </div>
+ 
+  </div>
 
 </body>
 </html>
