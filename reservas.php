9c95e790827397f62f7870d5ea8e768b5b60e491
Neri <bosconerina06@gmail.com>
2024-11-07 09:58:41 -0300

    Dale marti deja de programar en la misma pag

--- reservas.php
+++ reservas.php
@@ -11,6 +11,13 @@
 <body>
 <?php include('template/header.php') ?>
     
+<div class="siseve">
+</div>
+<div class="todo">
+    <div>
+        <h1>Mis reservas</h1>
+    </div>
 
+</div>
 </body>
 </html>
