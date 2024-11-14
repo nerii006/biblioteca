07848c52c7a7397e4e112fd2e3e42e3ebb7754a4
martimil <barnardmartinao@gmail.com>
2024-11-14 08:58:10 -0300

    Avances de la dupla

--- inicioSesion.php
+++ inicioSesion.php
@@ -13,20 +13,36 @@
     <link rel="stylesheet" href="./estilos/nav.css">
     <link rel="stylesheet" href="./estilos/form.css">
     <link rel="stylesheet" href="./estilos/footer.css">
+
 </head>
 <?php
     if ($_POST) {
-       /*  $query = $conexion->prepare("SELECT * FROM alumnos");
+       /*$query = $conexion->prepare("SELECT * FROM alumnos");
         $query->execute();
         $usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
  */
-        $query = $conexion->prepare("INSERT INTO alumnos(nombre_alumno, dni_alumno, contrasena_alumno) VALUES (:username, :dni, :clave)");
+
+        $query = $conexion->prepare("SELECT * FROM alumnos");
+        $query->execute();
+        $alumnos = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        
+       foreach ($alumnos as $alumno) {
+        if(($_POST['password'] == $alumno['contrasena_alumno']) && ($_POST['dni'] == $alumno['dni_alumno'])) {
+            echo "Inicio de sesion";
+        } else {
+            echo "ERRORR!!";
+        }
+       } 
+
+
+       /*  $query = $conexion->prepare("INSERT INTO alumnos(nombre_alumno, dni_alumno, contrasena_alumno) VALUES (:username, :dni, :clave)");
         $query->bindparam(":username", $_POST['username']);
         $query->bindparam(":dni", $_POST['dni']);
         $query->bindparam(":clave", $_POST['password']);
         $query->execute();
 
-        header('Location:libros.php');
+        header('Location:libros.php'); */
     }
 ?>
 
@@ -39,11 +55,7 @@
 
     
         <form method="post" class="form">
-            <h2>Registrate</h2> 
-            <label for="Nombre">Nombre de usuario:</label>
-            <input type="text" name="username" id="username" placeholder="Nombre de Usuario" required>
-            <br>
-
+            <h2>Inicio de Sesión</h2> 
             <label for="DNI">DNI:</label>
             <input type="text" name="dni" id="dni" placeholder="DNI" required>
             <br>
@@ -51,9 +63,9 @@
             <label for="Contraseña">Contraseña:</label>
             <input type="password" name="password" id="password" placeholder="Contraseña" required>
             <br>
-            <button type="submit">Registrarse</button>
+            <button type="submit">Iniciar sesión</button>
             <br>
-            <h4>Si ya tienes una cuenta haz <a href="inicioSesion.php">Click aquí</a> para Iniciar sesión</h4>
+            <h4>Si no tienes una cuenta haz <a href="index.php">Click aquí</a> para regiistrarse</h4>
         </form> 
 
     </div>
